c373254ff8590c3b8562c11783c06a3ca332913e
Xhoel Permeti <xh.permeti@gmail.com>
2025-04-23 22:04:33 +0100

    Implemented the functionalities of the horse class such as: fall, get/set confidence, get distance traveled, get name, get/set symbol, go back to start, has fallen, and move forward.

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -2,14 +2,17 @@
 /**
  * Write a description of class Horse here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Xhoel Permeti 
+ * @version 1.0
  */
 public class Horse
 {
     //Fields of class Horse
-    
-    
+    private String name;
+    private char symbol;
+    private int distanceTravelled;
+    private boolean hasFallen;
+    private double confidence;
       
     //Constructor of class Horse
     /**
@@ -17,60 +20,103 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+        symbol = horseSymbol;
+        name = horseName;
+        setConfidence(horseConfidence); 
+        distanceTravelled = 0;
+        hasFallen = false;
     }
     
     
     
     //Other methods of class Horse
+
+    /**
+     * Marks this horse as fallen.
+     */
     public void fall()
     {
-        
+        hasFallen = true;
+        // slightly decrease confidence once the horse has fallen
+        confidence = Math.max(0, confidence - 0.1); 
     }
     
+    /**
+     * Returns the confidence rating of the horse.
+     */
     public double getConfidence()
     {
-        
+        return confidence;
     }
     
+    /**
+     * Returns the distance the horse has travelled so far.
+     */
     public int getDistanceTravelled()
     {
-        
+        return distanceTravelled;
     }
     
+    /**
+     * Returns the name of the horse.
+     */
     public String getName()
     {
-        
+        return name;   
     }
     
+    /**
+     * Returns the symbol used to represent the horse.
+     */
     public char getSymbol()
     {
-        
+        return symbol;
     }
     
+     /**
+     * Resets the horse’s state to the beginning of the race.
+     */
     public void goBackToStart()
     {
-        
+        distanceTravelled = 0;
+        hasFallen = false;
     }
     
+    /**
+     * Checks if the horse has fallen.
+     */
     public boolean hasFallen()
     {
-        
+        return hasFallen;
     }
 
+    /**
+     * Moves the horse forward by 1 unit.
+     */
     public void moveForward()
     {
-        
+        distanceTravelled++;
     }
 
+    /** accounts for errors
+     * Sets a new confidence rating for the horse.
+     */
     public void setConfidence(double newConfidence)
     {
-        
+        if (newConfidence < 0) {
+            confidence = 0;
+        } else if (newConfidence > 1) {
+            confidence = 1;
+        } else {
+            confidence = newConfidence;
+        }
     }
     
+    /**
+     * Sets a new symbol for the horse.
+     */
     public void setSymbol(char newSymbol)
     {
-        
+        symbol = newSymbol;
     }
-    
 }
